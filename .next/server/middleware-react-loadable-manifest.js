44d1c8d23e40c9f889d274e5a7074e79c30cec2a
Matthew Starks <ukandit@hotmail.com>
2024-12-24 13:12:51 -0500

    reverted

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST='{"node_modules\\\\next\\\\dist\\\\client\\\\index.js -> ../pages/_app":{"id":5203,"files":["static/chunks/203.8936f7750e167052.js"]},"node_modules\\\\next\\\\dist\\\\client\\\\index.js -> ../pages/_error":{"id":6218,"files":["static/chunks/218.b1effa43d3584634.js"]}}';
