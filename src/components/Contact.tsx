00a6d4d48e3d20e4a8d1f77470b5c3886857ca9a
Matthew Starks <ukandit@hotmail.com>
2025-01-11 13:33:38 -0500

    disabled contact me

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -53,7 +53,7 @@ function Contact() {
     <div id="contact">
       <div className="items-container">
         <div className="contact_wrapper">
-          <h1>Contact Me</h1>
+          <h1>Contact Me (Disabled)</h1>
           <p>Got a project waiting to be realized? Let's collaborate and make it happen!</p>
           <Box
             ref={form}
